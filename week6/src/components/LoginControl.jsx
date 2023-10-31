fb691f57b3e10b57ed71797e5b13893bea563e74
anfdnfl@naver.com <anfdnfl@khu.ac.kr>
2023-10-31 22:49:58 +0900

    week6-Mission & improvement

--- week6/src/components/LoginControl.jsx
+++ week6/src/components/LoginControl.jsx
@@ -1,75 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as S from './LoginControlStyled';
 
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLoggedIn: false };
+function LoginControl() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+  const handleLoginClick = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogoutClick = () => {
+    setIsLoggedIn(false);
+  };
+
+  let button;
+  let welcomeMessage;
+
+  if (isLoggedIn) {
+    button = <S.loginBtn onClick={handleLogoutClick}>로그아웃</S.loginBtn>;
+    welcomeMessage = <S.welMsg>환영합니다!</S.welMsg>;
+  } else {
+    button = <S.loginBtn onClick={handleLoginClick}>로그인</S.loginBtn>;
+    welcomeMessage = <S.welMsg>로그인 해주세요!</S.welMsg>;
   }
 
-  handleLoginClick() {
-    this.setState({ isLoggedIn: true });
-  }
-
-  handleLogoutClick() {
-    this.setState({ isLoggedIn: false });
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
-    let welcomeMessage;
-
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-      welcomeMessage = <div style={{ color: 'white' }}>환영합니다!</div>;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-      welcomeMessage = <div style={{ color: 'white' }}>로그인 해주세요!</div>;
-    }
-
-    return (
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        {button}
-        {welcomeMessage}
-      </div>
-    );
-  }
-}
-
-function LoginButton(props) {
   return (
-    <button
-      style={{
-        marginRight: '20px',
-        border: '1px solid black',
-        width: '70px',
-        height: '25px',
-        borderRadius: '11px',
-      }}
-      onClick={props.onClick}
-    >
-      로그인
-    </button>
-  );
-}
-
-function LogoutButton(props) {
-  return (
-    <button
-      style={{
-        marginRight: '20px',
-        border: '1px solid black',
-        width: '70px',
-        height: '25px',
-        borderRadius: '11px',
-      }}
-      onClick={props.onClick}
-    >
-      로그아웃
-    </button>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      {button}
+      {welcomeMessage}
+    </div>
   );
 }
 
---
+++ week6/src/components/LoginControlStyled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const welMsg = styled.div`
+  color: white;
+`;
+
+export const loginBtn = styled.button`
+  margin-right: 20px;
+  border: 1px solid black;
+  width: 70px;
+  height: 25px;
+  border-radius: 11px;
+`;
